62085aa29191ab0bacbb8995c0c9bdd996f750e1
FreddyRE <freddy_romero@hotmail.com>
2025-01-11 21:39:21 -0600

    chore: add scaffolding for initial project structure

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import "./App.css"
+import { Logos } from "./Logos"
+export const App = () => {
+    return(
+        <div className="app">
+            <Logos/>
+        </div>
+    )
+   
+}
+
---
+++ src/Components/Logos.js
@@ -0,0 +1,16 @@
+import { Logo } from "./Logo"
+import "./Logos.css"
+
+export const Logos  = () => {
+    return(
+        <div className="logos">
+            <Logo />
+            <Logo />
+            <Logo />
+            <Logo />
+            <Logo />
+            <Logo />
+
+        </div>
+    )
+}
